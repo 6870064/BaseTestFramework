a55ab85b63a3f2d8b259239ccd5800f542ca5947
Dzmitry Viachaslavau <6870064@gmail.com>
2023-07-05 22:05:00 +0200

    Initial commit

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,19 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import utils.JSExecutor;
 import utils.PropertyReader;
 
 import static tests.BaseTest.driver;
 
 //@Log4j2
 public abstract class BasePage {
-    public static JSExecutor jsExecutor;
+  //  public static JSExecutor jsExecutor;
     protected PropertyReader propertyReader = new PropertyReader("src/test/resources/configuration.properties");
     public final String BASE_URL = propertyReader.getPropertyValueByKey("base.url");
 
     public BasePage(WebDriver driver) {
-        jsExecutor = new JSExecutor(driver);
+     //   jsExecutor = new JSExecutor(driver);
     }
 
     public abstract boolean isPageOpened();
